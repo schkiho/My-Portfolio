873393654b7a119478950c5b7d88477a5131f1ba
schkiho <schkiho@yahoo.de>
2018-10-06 21:20:04 +0200

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,9 @@
-//select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 
 const navItems = document.querySelectorAll(".nav-item");
-
-// Set Initial State Of Menu
 
 let showMenu = false;
 
@@ -20,7 +17,6 @@ function toggleMenu() {
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
-    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
@@ -29,7 +25,6 @@ function toggleMenu() {
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
-    // Set Menu State
     showMenu = false;
   }
 }
